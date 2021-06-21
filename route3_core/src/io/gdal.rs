d7d393aa051748e9186138867731e6b0de1a4d86
Nico Mandery <nico@nmandery.net>
2021-06-21 20:05:01 +0200

    gdal: write-support for generic weight types

--- route3_core/src/io/gdal.rs
+++ route3_core/src/io/gdal.rs
@@ -1,7 +1,5 @@
-use std::ops::Add;
-
-use eyre::Result;
-use gdal::vector::{Defn, Feature, OGRwkbGeometryType, ToGdal};
+use gdal::errors::Result;
+use gdal::vector::{Defn, Feature, FieldDefn, Layer, OGRFieldType, OGRwkbGeometryType, ToGdal};
 use gdal::Driver;
 use geo_types::LineString;
 use h3ron::ToCoordinate;
@@ -13,9 +11,82 @@ pub trait OgrWrite {
         -> Result<()>;
 }
 
+pub trait WeightFeatureField {
+    fn register_weight_fields(layer: &Layer) -> Result<()>;
+    fn fill_weight_feature_fields<'a>(&self, feature: &'a mut Feature) -> Result<()>;
+}
+
+pub const WEIGHT_FIELD_NAME: &'static str = "weight";
+
+impl WeightFeatureField for u64 {
+    fn register_weight_fields(layer: &Layer) -> Result<()> {
+        let weight_field_defn = FieldDefn::new(WEIGHT_FIELD_NAME, OGRFieldType::OFTInteger64)?;
+        weight_field_defn.add_to_layer(&layer)
+    }
+
+    fn fill_weight_feature_fields<'a>(&self, feature: &mut Feature<'a>) -> Result<()> {
+        feature.set_field_integer64(WEIGHT_FIELD_NAME, *self as i64)
+    }
+}
+
+impl WeightFeatureField for i64 {
+    fn register_weight_fields(layer: &Layer) -> Result<()> {
+        let weight_field_defn = FieldDefn::new(WEIGHT_FIELD_NAME, OGRFieldType::OFTInteger64)?;
+        weight_field_defn.add_to_layer(&layer)
+    }
+
+    fn fill_weight_feature_fields<'a>(&self, feature: &mut Feature<'a>) -> Result<()> {
+        feature.set_field_integer64(WEIGHT_FIELD_NAME, *self)
+    }
+}
+
+impl WeightFeatureField for f64 {
+    fn register_weight_fields(layer: &Layer) -> Result<()> {
+        let weight_field_defn = FieldDefn::new(WEIGHT_FIELD_NAME, OGRFieldType::OFTReal)?;
+        weight_field_defn.add_to_layer(&layer)
+    }
+
+    fn fill_weight_feature_fields<'a>(&self, feature: &mut Feature<'a>) -> Result<()> {
+        feature.set_field_double(WEIGHT_FIELD_NAME, *self)
+    }
+}
+
+impl WeightFeatureField for f32 {
+    fn register_weight_fields(layer: &Layer) -> Result<()> {
+        let weight_field_defn = FieldDefn::new(WEIGHT_FIELD_NAME, OGRFieldType::OFTReal)?;
+        weight_field_defn.add_to_layer(&layer)
+    }
+
+    fn fill_weight_feature_fields<'a>(&self, feature: &mut Feature<'a>) -> Result<()> {
+        feature.set_field_double(WEIGHT_FIELD_NAME, *self as f64)
+    }
+}
+
+impl WeightFeatureField for i32 {
+    fn register_weight_fields(layer: &Layer) -> Result<()> {
+        let weight_field_defn = FieldDefn::new(WEIGHT_FIELD_NAME, OGRFieldType::OFTInteger)?;
+        weight_field_defn.add_to_layer(&layer)
+    }
+
+    fn fill_weight_feature_fields<'a>(&self, feature: &mut Feature<'a>) -> Result<()> {
+        feature.set_field_integer(WEIGHT_FIELD_NAME, *self as i32)
+    }
+}
+
+impl WeightFeatureField for u32 {
+    fn register_weight_fields(layer: &Layer) -> Result<()> {
+        let weight_field_defn = FieldDefn::new(WEIGHT_FIELD_NAME, OGRFieldType::OFTInteger)?;
+        weight_field_defn.add_to_layer(&layer)
+    }
+
+    fn fill_weight_feature_fields<'a>(&self, feature: &mut Feature<'a>) -> Result<()> {
+        feature.set_field_integer(WEIGHT_FIELD_NAME, *self as i32)
+    }
+}
+
 impl<T> OgrWrite for H3Graph<T>
 where
-    T: PartialOrd + PartialEq + Add + Copy,
+    T: WeightFeatureField,
 {
     fn ogr_write<S: AsRef<str>>(
         &self,
@@ -27,20 +98,18 @@ where
         let mut ds = drv.create_vector_only(output_name.as_ref())?;
 
         let lyr = ds.create_layer(layer_name.as_ref(), None, OGRwkbGeometryType::wkbLineString)?;
-
-        //let weight_field_defn = FieldDefn::new("weight", OGRFieldType::OFTInteger64)?;
-        //weight_field_defn.add_to_layer(&lyr)?;
+        T::register_weight_fields(&lyr)?;
 
         let defn = Defn::from_layer(&lyr);
 
-        for (edge, _) in self.edges.iter() {
+        for (edge, weight) in self.edges.iter() {
             let mut ft = Feature::new(&defn)?;
             let coords = vec![
                 edge.origin_index_unchecked().to_coordinate(),
                 edge.destination_index_unchecked().to_coordinate(),
             ];
             ft.set_geometry(LineString::from(coords).to_gdal()?)?;
-            //ft.set_field_integer64("weight", edge.weight as i64)?;
+            weight.fill_weight_feature_fields(&mut ft)?;
             ft.create(&lyr)?;
         }
         Ok(())
