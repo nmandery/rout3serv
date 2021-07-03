760b8584c192dc9cc800a3d47010b85513d9fb0f
Nico Mandery <nico@nmandery.net>
2021-07-03 18:58:41 +0200

    hashbrown

--- route3_core/src/lib.rs
+++ route3_core/src/lib.rs
@@ -1,4 +1,5 @@
-use std::collections::{HashMap, HashSet};
+//use std::collections::{HashMap, HashSet};
+use hashbrown::{HashMap, HashSet};
 
 // re-export core libraries for easier dependency management
 #[cfg(feature = "with-gdal")]
