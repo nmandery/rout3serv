return route wkbs. fix? cost calculation

route3_python_client/route3_client/__init__.py
@@ -2,15 +2,18 @@ __version__ = '0.1.0'
 
 from typing import Optional, Iterable
 
+import geopandas as gpd
 import grpc
-from shapely.geometry.base import BaseGeometry
-from shapely.geometry import Point
-import pyarrow as pa
 import pandas as pd
+import pyarrow as pa
+import numpy as np
 import shapely.wkb
+from shapely.geometry import Point
+from shapely.geometry.base import BaseGeometry
 
 from . import route3_pb2
-from .route3_pb2 import DisturbanceOfPopulationMovementResponse, RouteWKB
+from .route3_pb2 import DisturbanceOfPopulationMovementResponse, RouteWKB, \
+    GetDisturbanceOfPopulationMovementRoutesResponse
 from .route3_pb2_grpc import Route3Stub
 
 DEFAULT_PORT = 7088
@@ -65,6 +68,38 @@ class Server:
         req.dopm_id = dopm_id
         response = self.stub.GetDisturbanceOfPopulationMovement(req)
         return self._return_stats(response)
+
+    def get_disturbance_of_population_movement_routes(self, dopm_id: str, cells: Iterable[int]) -> gpd.GeoDataFrame:
+        req = route3_pb2.GetDisturbanceOfPopulationMovementRoutesRequest()
+        req.dopm_id = dopm_id
+        for cell in cells:
+            req.cells.append(cell)
+
+        response = self.stub.GetDisturbanceOfPopulationMovementRoutes(req)
+
+        print(len(response.routes_with_disturbance), len(response.routes_without_disturbance))
+        geoms = []
+        h3index_origin = []
+        h3index_destination = []
+        cost = []
+        with_disturbance_list = []
+        for with_disturbance, route_list in (
+        (1, response.routes_with_disturbance), (0, response.routes_without_disturbance)):
+            for route in route_list:
+                h3index_origin.append(route.origin_cell)
+                h3index_destination.append(route.destination_cell)
+                with_disturbance_list.append(with_disturbance)
+                cost.append(route.cost)
+                geoms.append(shapely.wkb.loads(route.wkb))
+
+        gdf = gpd.GeoDataFrame({
+            "geometry": geoms,
+            "h3index_origin": np.asarray(h3index_origin, dtype=np.uint64),
+            "h3index_destination": np.asarray(h3index_destination, dtype=np.uint64),
+            "cost": np.asarray(cost, dtype=np.float64),
+            "with_disturbance": np.asarray(with_disturbance_list, dtype=np.int),
+        }, crs=4326)
+        return gdf
 
 
 def _routewkb_get_geometry(self):